fe71bb8437e7778d4cc566ecc7e6d4f15e82280c
Isaac Watts <Isaac.s.watts@gmail.com>
2025-06-05 20:07:33 -0400

    UserForm now is able to handle name and city_zip_code inputs from the user

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
+import { Dimensions } from 'react-native';
 
 export function UserForm() {
-  const [ [name, city_zip_code], setName] = useState("");
+  const [name, setName] = useState("");
+  const [city_zip_code, setCity] = useState("");
+
+  const windowWidth = Dimensions.get('window').width;
 
   const handleSubmit = () => {
-    Alert.alert(`The name you entered was: ${name}`);
+    console.log(`The name you entered was: ${name} and the zip code/city you entered is ${city_zip_code}`);
+    Alert.alert("Submission", `The name you entered was: ${name} and the zip code/city you entered is ${city_zip_code}`);
+    // TODO:
+    // Enter submit logic backend here using the supabase
   };
+  
 
   return (
     <View>
       <Text></Text>
       <TextInput
         value={city_zip_code}
-        onChangeText={setName}
+        onChangeText={setCity}
         placeholder="Enter your zip code or city"
         style={{
           borderWidth: 1,
           borderColor: "#ccc",
           padding: 10,
           marginBottom: 10,
+          width: windowWidth
         }}
       />
 
@@ -32,6 +41,7 @@ export function UserForm() {
           borderColor: "#ccc",
           padding: 10,
           marginBottom: 10,
+          width: windowWidth
         }}
       />
       <Button title="Submit" onPress={handleSubmit} />
