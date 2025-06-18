4ed5f88d39b799cf3948eb5fca76ac46839494ed
Isaac Watts <Isaac.s.watts@gmail.com>
2025-06-18 18:18:04 -0400

    Added hours of operations to a database

---
+++ db/hours_of_ops_insert.py
@@ -0,0 +1,17 @@
+import sqlalchemy as db
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Access environment variables
+database_url = os.getenv("DATABASE_URL")
+
+hours_of_ops_df = pd.read_excel("hours_of_operation.xlsx")
+print(hours_of_ops_df)
+
+engine = db.create_engine(database_url) # create a db engine to make a connection
+hours_of_ops_df.to_sql("hours_of_operation", engine, if_exists='replace') # replace the data if it already exist
+print("Database inserts complete!")
