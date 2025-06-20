f75cb4e07360696fae1fb1d131c9b5b5bd894294
Isaac Watts <Isaac.s.watts@gmail.com>
2025-06-20 07:05:45 -0400

    Code for embedding the data for the database

--- db/locations_insert.py
+++ db/locations_insert.py
@@ -1,4 +1,5 @@
 import sqlalchemy as db
+from openai import OpenAI
 import pandas as pd
 from dotenv import load_dotenv
 import os
@@ -6,11 +7,37 @@ import os
 # Load environment variables from the .env file
 load_dotenv()
 
+
 # Access environment variables
 database_url = os.getenv("DATABASE_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def embed(content):
+    response = client.embeddings.create(
+        input=content,
+        model="text-embedding-3-small"
+    )
+    return response.data[0].embedding
+
+
+
 
 organizations_df = pd.read_excel("Health_Care_Organizations_in_Petersburg_VA_Phrases.xlsx", sheet_name="Petersburg Facilities")
-phrases_df = pd.read_excel("Health_Care_Organizations_in_Petersburg_VA_Phrases.xlsx", sheet_name="Healthcare Phrases")
+organizations_df.columns = organizations_df.columns.str.strip()
 
-phrases_df = phrases_df[phrases_df["Long term Care"] != 0].dropna() # data cleaning for removing missing values
-phrases_df.to_csv("healthcare_phrases.csv", index=False)
+organizations_df['embedding'] = organizations_df.apply(lambda row: embed("{} specializes in {} with an address {} opened during {}"
+                                                                        .format(
+                                                                            row['Name of Organization'], 
+                                                                            row['Theme (Type)'], 
+                                                                            row['Address'], 
+                                                                            row['Hours'] )
+                                                                        ), 
+                                                                    axis=1)
+
+organizations_df.to_json("test.json" ,orient='index', indent=2)
+
+
+# engine = db.create_engine(database_url) # create a db engine to make a connection
+# organizations_df.to_sql("locations", engine, if_exists='replace', index=False) # replace the data if it already exist
+# print("Database inserts complete for locations!")
